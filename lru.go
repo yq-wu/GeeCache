cd4986462603b58fc30b9814947a1a7b1ff30f04
wuyq <wuyq2023@outlook.com>
2024-06-04 17:01:53 +0800

    first day

---
+++ lru.go
@@ -0,0 +1,69 @@
+package GeeCache
+
+import "container/list"
+
+type Cache struct {
+	maxBytes  int64
+	nBytes    int64
+	ll        *list.List
+	cache     map[string]*list.Element
+	OnEvicted func(key string, value Value)
+}
+
+type Value interface {
+	len() int
+}
+
+type Entry struct {
+	key   string
+	value Value
+}
+
+func New(maxBytes int64, OnEvicted func(key string, value Value)) *Cache {
+	return &Cache{
+		maxBytes:  maxBytes,
+		ll:        list.New(),
+		cache:     make(map[string]*list.Element),
+		OnEvicted: OnEvicted,
+	}
+}
+
+func (c *Cache) Get(key string) (value Value, ok bool) {
+	if ele, ok := c.cache[key]; ok {
+		c.ll.MoveToFront(ele)
+		kv := ele.Value.(*Entry)
+		return kv.value, true
+	}
+	return
+}
+func (c *Cache) RemoveOldest() {
+	ele := c.ll.Back()
+	if ele != nil {
+		c.ll.Remove(ele)
+		kv := ele.Value.(*Entry)
+		delete(c.cache, kv.key)
+		c.nBytes -= int64(len(kv.key)) + int64(kv.value.len())
+		if c.OnEvicted != nil {
+			c.OnEvicted(kv.key, kv.value)
+		}
+	}
+}
+
+func (c *Cache) Add(key string, value Value) {
+	if ele, ok := c.cache[key]; ok {
+		kv := ele.Value.(*Entry)
+		c.ll.MoveToFront(ele)
+		c.nBytes += int64(value.len()) - int64(kv.value.len())
+		kv.value = value
+	} else {
+		ele := c.ll.PushFront(&Entry{
+			key:   key,
+			value: value,
+		})
+		c.nBytes += int64(value.len()) + int64(len(key))
+		c.cache[key] = ele
+	}
+	for c.maxBytes != 0 && c.maxBytes < c.nBytes {
+		c.RemoveOldest()
+	}
+}
